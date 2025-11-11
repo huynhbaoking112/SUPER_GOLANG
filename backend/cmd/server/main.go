500b1a65554293faa625de5e3521b82a9e153c6e
Huynh Bao King <119712154+huynhbaoking112@users.noreply.github.com>
2025-11-11 19:56:07 +0700

    baoking/chore:  add subdomain

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -22,7 +22,7 @@ func main() {
 	})
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173, http://127.0.0.1:5173",
+		AllowOrigins:     "http://localhost:5173, http://127.0.0.1:5173, https://saphabot.me, http://saphabot.me",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Requested-With, X-Organization-ID,X-User-ID",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true, // Chỉ bật khi cần thiết
